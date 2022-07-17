de718d6920882dc4bfac161a22ba1ca1a1ffcdf5
Denis Yavorskiy <svirgibs@gmail.com>
2022-07-17 17:12:12 +0300

    Добавление новой фикстуры. Добавление нового теста "логин".

---
+++ fixture/application.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        else:
+            raise ValueError("Unrezognized browser %s" % browser)
+        self.wd.implicitly_wait(2)
+        self.session = SessionHelper(self)
+        self.base_url =base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if not wd.current_url.endswith("/addressbook/"):
+            wd.get(self.base_url)
+
+    def change_field_value(self, field_name, text):
+        wd = self.wd
+        if text is not None:
+            wd.find_element("name", field_name).clear()
+            wd.find_element("name", field_name).send_keys(text)
+
+    def destroy(self):
+        self.wd.quit()
